faa1030f4a7f85aa8b05b498c009ef3710ec6bf3
halx99 <halx99@users.noreply.github.com>
2022-04-02 05:52:13 +0000

    deploy: 0ea2db2dfd3fe5bffb4f257df9f94739700f5c5e

---
+++ navtreedata.js
@@ -0,0 +1,83 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "adxe", "index.html", [
+    [ "About adxe", "index.html#sec1", null ],
+    [ "License", "index.html#sec2", null ],
+    [ "Get the source code", "index.html#sec3", null ],
+    [ "How to contribute", "index.html#sec4", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespace Members", "namespacemembers.html", [
+      [ "All", "namespacemembers.html", null ],
+      [ "Functions", "namespacemembers_func.html", null ],
+      [ "Variables", "namespacemembers_vars.html", null ],
+      [ "Typedefs", "namespacemembers_type.html", null ],
+      [ "Enumerations", "namespacemembers_enum.html", null ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d0/dce/classcocos2d_1_1_menu.html#a9bc3fb695690f6b9a722415004eefb24",
+"d1/d9d/classcocos2d_1_1_t_m_x_tile_anim_manager.html#aebbdbcd6798148f3fb5f71d011d76032",
+"d2/dad/classcocos2d_1_1_vec4.html#ac4147ef96b0b68f8e696804425f350dd",
+"d3/d5c/classcocos2d_1_1_sprite.html#a1fea730dc21b4a19b56ab386c88ce19e",
+"d3/d82/classcocos2d_1_1_node.html#afcc9bd4bfa3a632666054886fe4fefc1",
+"d4/d7d/classcocos2d_1_1ui_1_1_slider.html#a2d13a93fb5c688d2fe2d656dc6faa601",
+"d5/d8c/classcocos2d_1_1_i_m_e_dispatcher.html#a83ec88fd45de349d949c103981d60a13",
+"d6/d7c/classcocos2d_1_1_move_by.html#a9404cb2425bc3490a597b177dd9e98af",
+"d7/d76/classcocos2d_1_1_sprite_frame_cache.html#a3e78975b36ef8daef05e541787de53aa",
+"d8/d23/classcocos2d_1_1ui_1_1_tab_control.html#a714cd387535b2d6299466beab45b4cfa",
+"d8/d62/classcocos2d_1_1ui_1_1_widget.html#ac1bacd1026bbbdc8901c996b56dd797b",
+"d9/d9c/classcocos2d_1_1ui_1_1_page_view.html#a1d8106c70579c89450317ce6f585fd8c",
+"da/d76/classcocos2d_1_1_render_queue.html#ac8bb3912a3ce86b15842e79d0b421204",
+"db/d61/classcocos2d_1_1_action.html#ac8cae2d59252ed4c924a6c0ec86527db",
+"db/dd9/classcocos2d_1_1_particle_system.html#aa80fe4e9a559009407475c9587214b48",
+"dc/d16/classcocos2d_1_1_ripple3_d.html#aaea87b1eb080be4d1cbb675c24eb5729",
+"dc/d95/classcocos2d_1_1_text_field_t_t_f.html#a82ea630c2b42d51ac9774f8209bd8e35",
+"dd/d4c/classcocos2d_1_1backend_1_1_depth_stencil_state_g_l.html#a7d2ef6d6ede2ee89daa9b0a295df0709",
+"de/d36/structcocos2d_1_1_v3_f___c4_b___t2_f___quad.html#a531c4ed802f12a98c523603386f668f1",
+"df/d05/classcocos2d_1_1_repeat.html#a82f24562dbde467eaa45c5a075678bd3",
+"df/d76/classcocos2d_1_1ui_1_1_edit_box.html#abe506910f1644dfa42957fc45d51289b"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
